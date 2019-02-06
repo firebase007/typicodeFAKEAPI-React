69d4a93aa50d5edcfe1789c42ce11c9ba1c5ef52
Alexander <alex.nnakwue@gmail.com>
2019-02-06 17:26:31 +0100

    Header touched

--- src/Components/Login.js
+++ src/Components/Login.js
@@ -3,9 +3,6 @@ import React, { Component } from "react";
 class Login extends Component {
   constructor(props) {
     super(props);
-
-    console.log(this.props);
-
     this.state = {
       userName: "",
       users: []
--- src/Components/User.js
+++ src/Components/User.js
@@ -1,15 +1,18 @@
 import React from "react";
+import { Jumbotron, Button, Fade, Container } from "reactstrap";
+import styled from "styled-components";
 import { Redirect } from "react-router-dom";
 import PostList from './PostList';
 
+
+const ButtonWrapper = styled.div`
+  margin-right: 20px;
+`;
 class User extends React.Component {
   constructor(props) {
     super(props);
     console.log(this.props);
-    this.state = {
-      user: null,
-      isLoggedIn: true
-    };
+    this.state = { user: null, isLoggedIn: true, fadeIn: true };
   }
 
   componentDidMount() {
@@ -27,47 +30,59 @@ class User extends React.Component {
       localStorage.removeItem("isLoggedin");
       history.push("/login");
     };
+  
+  toggle = () => {
+    this.setState({ fadeIn: !this.state.fadeIn });
+  }
 
   render() {
-    const { user, isLoggedIn } = this.state;
+    const { user, isLoggedIn, fadeIn } = this.state;
     // if (!isLoggedIn) {
     //   return <Redirect to="/login" />;
     // }
     if (!user) {
       return <div>Loading ...</div>;
     }
-    return (
-      <div className="">
-        <button onClick={this.handleLogout} className="">
-          Logout
-        </button>{" "}
-        <div classsName="">
-          <h1 className="">Welcome, {user.name}</h1>
-        </div>
-        <div
-          style={{
-            border: "1px solid grey",
-            maxWidth: "450px",
-            marginBottom: "10px"
-          }}
-        >
-          <div>
-            Website: <a href="/">{user.website}</a>
-          </div>
-          <div>
-            email: {user.email}
-            <div>
-              Address: {user.address.street}, {user.address.suite},{" "}
-              {user.address.city}, {user.address.zipcode}
-            </div>
-            <div>Phone: {user.phone}</div>
-          </div>
-          <div>Company details: {user.company.name}</div>
-        </div>{" "}
-        
-            <PostList currentLoggedInUser={user}/>
-      </div>
-    );
+    return <div style={{ minHeight: "100px", padding: "5em inherit" }}>
+        <Jumbotron fluid>
+          <Container fluid>
+            <ButtonWrapper>
+              <Button onClick={this.handleLogout} className="" color="secondary">
+                Logout
+              </Button>
+            </ButtonWrapper>
+            <h1 className="display-3">Welcome, {user.name}</h1>
+            <hr className="my-2" />
+            <p className="lead">
+              <Button color="primary" onClick={this.toggle}>
+                Preview Personal Info
+              </Button>
+            </p>
+            {fadeIn ? <Fade in={fadeIn} tag="h5" className="mt-3">
+                <div style={{ border: "1px solid grey", maxWidth: "420px", marginBottom: "15px", padding:"5px" }}>
+                  <div>
+                    website: <a href="/">{user.website}</a>
+                  </div>
+                  <div>
+                    e-mail: {user.email}
+                    <div>
+                      address: {user.address.street}, {user.address.suite}, {user.address.city}, {user.address.zipcode}
+                    </div>
+                    <div>phone no: {user.phone}</div>
+                  </div>
+                  <div>company details: {user.company.name}</div>
+                </div>
+              </Fade> : <span>
+                {" "}
+                <h6>
+                  Click on the button above to preview your personal
+                  information
+                </h6>
+              </span>}
+          </Container>
+        </Jumbotron>
+        <PostList currentLoggedInUser={user} />
+      </div>;
   }
 }
 
